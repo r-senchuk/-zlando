d5681cdc0697b9eab4583b9b9b574c3206fc5b3d
r-senchuk <frspm.roman@gmail.com>
2023-01-10 16:02:31 +0200

    cleanup

--- src/rinit.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import Button from "components/like_button"
-
-const e = React.createElement;
-
-const domContainer = document.querySelector('#reacontainer');
-const root = ReactDOM.createRoot(domContainer);
-root.render(e(Button));
